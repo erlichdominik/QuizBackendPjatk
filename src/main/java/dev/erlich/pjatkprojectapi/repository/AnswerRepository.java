57ad4b77f037476de46a23d0a02334e5b8981b92
MTomys <michalltomys@protonmail.ch>
2022-06-02 12:01:10 +0200

    Add repository annotations

--- src/main/java/dev/erlich/pjatkprojectapi/repository/AnswerRepository.java
+++ src/main/java/dev/erlich/pjatkprojectapi/repository/AnswerRepository.java
@@ -2,6 +2,8 @@ package dev.erlich.pjatkprojectapi.repository;
 
 import dev.erlich.pjatkprojectapi.model.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 }
